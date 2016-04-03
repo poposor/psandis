d39621cbc634fc83a5301fe4469f5855e12a0c5b
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-04-03 16:45:29 -0600

    Cards

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var http = require('http');
 var psandis = require('./psandis');
 var fs = require('fs');
+var nodestatic = require('node-static');
 
 var PORT = 7726;
+
+var files = new nodestatic.Server("static");
 
 http.createServer(function(req, res) {
 	var spl = req.url.substring(1).split("/");
 	if(spl[0] == "image") {
-		psandis.getCardImagePath(spl[1], function(path) {
+		var svgCallback = function(path) {
 			fs.readFile(path, function(err, data) {
 				if(err) {
 					res.writeHead(500, {"Content-type": "text/plain"});
 					res.write("Error loading file.");
+					res.end();
 				}
 				else {
 					res.writeHead(200, {"Content-type": "image/svg+xml"});
@@ -19,6 +23,24 @@ http.createServer(function(req, res) {
 					res.end();
 				}
 			});
-		});
+		};
+		if(spl[1] == "type") {
+			psandis.getTypeImagePath(spl[2], svgCallback);
+		}
+		else {
+			psandis.getCardImagePath(spl[2], svgCallback);
+		}
+	}
+	else if(spl[0] == "card") {
+		res.writeHead(200, {"Content-type": "text/html"});
+		res.write("<html><head>");
+		res.write('<link rel="stylesheet" href="/main.css"></style>');
+		res.write("</head><body>");
+		res.write(psandis.getCardHTML(spl[1]));
+		res.write("</body></html>");
+		res.end();
+	}
+	else {
+		files.serve(req, res);
 	}
 }).listen(PORT);
--- psandis.js
+++ psandis.js
@@ -1,8 +1,7 @@
 var YAML = require('yamljs');
 var fs = require('fs');
 
-var getCardImagePath = function(name, callback) {
-	var filename = 'data/cards/'+name+'.svg';
+var getImagePath = function(filename, callback) {
 	fs.stat(filename, function(err, stats) {
 		if(err || !stats.isFile()) {
 			filename = 'data/missing.svg';
@@ -11,11 +10,62 @@ var getCardImagePath = function(name, callback) {
 	});
 };
 
+var getCardImagePath = function(name, callback) {
+	getImagePath('data/cards/'+name+'.svg', callback);
+};
+
+var getTypeImagePath = function(name, callback) {
+	getImagePath('data/types/'+name+'.svg', callback);
+};
+
 var getCardData = function(name) {
 	return YAML.load('data/cards/'+name+'.yaml');
 };
 
+var getCardHTML = function(name, callback) {
+	var data = getCardData(name);
+	var tr = "";
+	tr += '<div class="ps-card">';
+	if("Health" in data) {
+		tr += '<span class="ps-topright">';
+		tr += '<span class="ps-health">'+data.Health+'</span>';
+		tr += '<img src="/image/type/'+data.Type+'" class="ps-type ps-type-main" />';
+		tr += '</span>';
+	}
+	tr += '<span class="ps-name">'+data.Name+'</span><br />';
+	tr += '<div class="ps-image-area">';
+	tr += '<img src="/image/card/'+name+'" class="ps-image" />';
+	tr += '</div>';
+	if("Info" in data) {
+		tr += '<span class="ps-info ps-card-info">'+data.Info+'</span><br />';
+	}
+	if("Moves" in data) {
+		var moves = data.Moves;
+		for(var i = 0; i < moves.length; i++) {
+			var move = moves[i];
+			tr += '<div class="ps-move">';
+			tr += '<img class="ps-type ps-type-move" src="/image/type/'+move.Type+'" />';
+			tr += '<span class="ps-move-name">'+move.Name+'</span>';
+			if("Damage" in move) {
+				tr += '<span class="ps-move-damage">'+move.Damage+'</span>';
+			}
+			tr += "<br />";
+			if("Info" in move) {
+				tr += '<span class="ps-info ps-move-info">'+move.Info+'</span>';
+			}
+			if("Recoil" in move) {
+				tr += '<span class="ps-move-recoil">-'+move.Recoil+'</span>';
+			}
+			tr += '</div>';
+		}
+	}
+	tr += '</div>';
+	return tr;
+};
+
 module.exports = {
 	getCardImagePath: getCardImagePath,
-	getCardData: getCardData
+	getTypeImagePath: getTypeImagePath,
+	getCardData: getCardData,
+	getCardHTML: getCardHTML
 };
