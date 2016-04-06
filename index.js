0bb0d299eb59464e9f3206e33ae9addcb062198f
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-04-06 17:33:10 -0600

    Card printing

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var PORT = 7726;
 
 var files = new nodestatic.Server("static");
 
+var writeHeader = function(res) {
+	res.writeHead(200, {"Content-type": "text/html"});
+	res.write("<html><head>");
+	res.write('<link rel="stylesheet" href="/main.css"></style>');
+	res.write("</head><body>");
+};
+
 http.createServer(function(req, res) {
 	var spl = req.url.substring(1).split("/");
 	if(spl[0] == "image") {
@@ -32,13 +39,47 @@ http.createServer(function(req, res) {
 		}
 	}
 	else if(spl[0] == "card") {
-		res.writeHead(200, {"Content-type": "text/html"});
-		res.write("<html><head>");
-		res.write('<link rel="stylesheet" href="/main.css"></style>');
-		res.write("</head><body>");
+		writeHeader(res);
 		res.write(psandis.getCardHTML(spl[1]));
 		res.write("</body></html>");
 		res.end();
+	}
+	else if(spl[0] == "cards") {
+		fs.readdir("data/cards", function(err, files) {
+			var cards = [];
+			for(var i = 0; i < files.length; i++) {
+				var sp = files[i].split(".");
+				if(sp.length == 2 && sp[1] == "yaml") {
+					cards.push(sp[0]);
+				}
+			}
+			var selected;
+			if(spl[1] == "page") {
+				selected = cards.slice(spl[2]*9, spl[2]*9+9);
+			}
+			else {
+				selected = cards.sort(function(){return Math.random()-0.5;}).slice(0,9);
+			}
+			console.log(selected);
+			writeHeader(res);
+			res.write("<table border=1>");
+			var WIDTH = 3;
+			for(var y = 0; y < 3; y++) {
+				res.write("<tr>");
+				for(var x = 0; x < WIDTH; x++) {
+					var i = y*WIDTH+x;
+					res.write('<td class="ps-card-cell">');
+					if(selected.length > i) {
+						console.log(selected[i]);
+						res.write(psandis.getCardHTML(selected[i]));
+					}
+					res.write("</td>");
+				}
+				res.write("</tr>");
+			}
+			res.write("</table></body></html>");
+			res.end();
+		});
 	}
 	else {
 		files.serve(req, res);
